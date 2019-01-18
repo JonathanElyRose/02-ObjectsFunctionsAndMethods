e91e67231fa9229e0b1f9abec6e86e08750c500d
elyjh <elyjh@rose-hulman.edu>
2019-01-18 09:19:27 -0500

    DONE

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -88,7 +88,7 @@ def turtleboi(givenColor, givenThickness):
     window.close_on_mouse_click()
 
 ###############################################################################
-# TODO: 5.
+# DONE: 5.
 #   COMMIT-and-PUSH your work (after changing this TO-DO to DONE).
 #
 #   As a reminder, here is how you should do so:
